78811bbde2a49cc0bd77e31cf370af618ff1e019
siewmailomaigaii <90055520+siewmailomaigaii@users.noreply.github.com>
2021-09-03 23:50:57 +0800

    Add files via upload

---
+++ Personnal/Person.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package person;
+
+/**
+ *
+ * @author User
+ */
+
+
+public class Person(){
+    private String name;
+    private String 
+    
+    public Person(String name) {
+        this.name = name;
+    }   
+}
+       
