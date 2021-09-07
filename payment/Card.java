93bb604566a2be078ec5f999b2d80dfc533a5e7c
JEKaon <83861664+JEhew1227@users.noreply.github.com>
2021-09-07 21:13:11 +0800

    Add files via upload

---
+++ payment/Card.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package democonsole;
+
+import java.util.Calendar;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author JEhew
+ */
+public class Card {
+    
+    private String cardNum;
+    private Calendar expiryDate;
+    private int cvv;
+    private double balance;
+    
+    private boolean credit;
+    public static boolean validateCard;
+
+    public Card(String cardNum, Calendar expiryDate, int cvv) {
+        this.cardNum = cardNum;
+        this.expiryDate = expiryDate;
+        this.cvv = cvv;
+        //this.balance = payment.generateRandomBalance();
+    }
+
+    public String toString(){
+        return String.format("%-20s %-15s %-5d ", cardNum,expiryDate,cvv);
+        
+    }   
+    
+    public static boolean validateCard(Card card) {
+        Pattern p = Pattern.compile("^\\d{4} \\d{4} \\d{4} \\d{4}$");
+        Matcher m = p.matcher(card.cardNum);
+        boolean validCardNum = m.matches();
+
+        Calendar c = Calendar.getInstance();
+        boolean validDate = (c.get(Calendar.DAY_OF_MONTH) >= card.expiryDate.get(Calendar.DAY_OF_MONTH));
+
+        boolean validCvv = Integer.toString(card.cvv).length() == 3;
+
+        return validCardNum && validDate && validCvv;
+    }
+    
+}
